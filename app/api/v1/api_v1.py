73bfe1c20076fbe475c49fbd5e38e2c408d85142
julio-sa <julio.ccbeu@gmail.com>
2025-11-03 21:24:11 -0300

    delete api_v1.py, auth.py, reports.py, database.py, auth.py, widgets.py

--- app/api/v1/api_v1.py
+++
@@ -1,8 +0,0 @@
-from fastapi import APIRouter
-
-from app.api.v1.routes import auth, reports, widgets
-
-api_router = APIRouter()
-api_router.include_router(widgets.router)
-api_router.include_router(reports.router)
-api_router.include_router(auth.router)
